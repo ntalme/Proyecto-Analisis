204522358ef8d0b25863538f12c8d97102b05ba9
ntalme <almendramanriquez@gmail.com>
2024-05-02 16:34:06 -0400

    aguegue el como imprimir en c

---
+++ Como impimir en C++.cpp
@@ -0,0 +1,14 @@
+//Primer programa en C++
+
+#include<iostream>
+
+
+using namespace std;
+
+int main(){
+
+    //Como imprimir
+    cout<<"Hola mundo :D"<<endl; //---> genera un espacio
+
+    return 0;
+}
