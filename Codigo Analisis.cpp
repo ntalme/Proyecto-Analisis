30a88c59b2088a256b839dca8030ef3949c4d7fb
ntalme <almendramanriquez@gmail.com>
2024-05-02 16:55:24 -0400

    prueba

---
+++ Codigo Analisis.cpp
@@ -0,0 +1,18 @@
+//hola esto es una prueba
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int numero;
+
+    cout<<"Digite un numeo: ";
+    //Como gaurdar un dato de un usuario
+    cin>>numero;
+
+    //Imprimir lo que puso:
+    cout<<"\nEl numero es: "<<numero;
+
+    return 0;
+}
